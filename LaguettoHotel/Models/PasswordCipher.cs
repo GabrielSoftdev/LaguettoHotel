2a384f9decea3d90ddac3c9eae437ef2ad77383d
Gabriel Valdivia <gabriel.softdev@gmail.com>
2021-11-24 10:52:34 -0300

    PasswordCipher: sha1 password encryption

---
+++ LaguettoHotel/Models/PasswordCipher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Windows.Input;
+
+namespace Hotel.Models
+{
+    public class PasswordCipher
+    {
+        public static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
